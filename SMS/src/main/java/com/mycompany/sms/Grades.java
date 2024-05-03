c34efc3dc6f419f2dd75cebdb6035f15757f2304
Jullian Sabando <166296896+jlnmx@users.noreply.github.com>
2024-05-03 21:01:11 +0800

    Update Grades.java

--- SMS/src/main/java/com/mycompany/sms/Grades.java
+++ SMS/src/main/java/com/mycompany/sms/Grades.java
@@ -9,36 +9,6 @@
  * @author delar
  */
 public class Grades {
-     int scores;
-     String name;
-     String subject;
-     
-     Grades(int scores, String name, String subject){
-         this.scores = scores;
-         this.name = name;
-         this.subject = subject;
-     }
-     
-    public void setScores(int scores){
-        this.scores = scores;   
-    }
     
-    public void setName(String name){
-        this.name = name;   
-    }
-    public void setSubject(String subject){
-       this.subject = subject;
-    }
-    
-    public int getScores (){
-        return scores;
-    }
-    
-    public String getName(){
-        return name;
-    }
-    public String getSubject(){
-        return subject;
-    }
    
 }
