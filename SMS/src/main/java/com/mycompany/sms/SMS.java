63ed481ba0432be8cebe86f95dd6fe0727650da4
SABANDO <saban@DESKTOP-7JC8R87>
2024-05-09 22:04:30 +0800

    Merge origin/master
    # Conflicts:
    #	SMS/src/main/java/com/mycompany/sms/Grades.java

--- SMS/src/main/java/com/mycompany/sms/SMS.java
+++ SMS/src/main/java/com/mycompany/sms/SMS.java
@@ -7,8 +7,8 @@ package com.mycompany.sms;
 public class SMS {
 
    public static void main(String[] args) {
-        new Student();              
-       
+        new Student();             
+        
     }
    
     }
--- SMS/src/main/java/com/mycompany/sms/Student.java
+++ SMS/src/main/java/com/mycompany/sms/Student.java
@@ -6,54 +6,70 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Student extends JFrame {
-    private JLabel lblName, lblStudentnum, lblAge, lblCourse;
-    private JTextField txtfldName = new JTextField();
+    private JLabel lblSurname, lblFirstname, lblStudentnum, lblAge, lblCourse, lblTitle;
+    private JTextField txtfldSurname = new JTextField();
+    private JTextField txtfldFirstname = new JTextField();
     private JTextField txtfldStudentnum = new JTextField();
     private JTextField txtfldAge = new JTextField();
     private JTextField txtfldCourse = new JTextField();
     private JButton btnSubmit;
-
+    
     public Student() {
         setTitle("Student Information");
         setLayout(null);
 
-        lblName = new JLabel("Name");
-        lblName.setBounds(20, 20, 100, 20);
-        lblName.setFont(new Font("Arial", Font.BOLD, 15));
+        lblTitle = new JLabel("Student Manager");
+        lblTitle.setBounds(150, 5, 200, 20);
+        lblTitle.setFont(new Font("Bell MT", Font.BOLD, 20));
+        lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
+
+        lblSurname = new JLabel("Surname");
+        lblSurname.setBounds(20, 40, 100, 20);
+        lblSurname.setFont(new Font("Arial", Font.BOLD, 15));
+
+        lblFirstname = new JLabel("First Name");
+        lblFirstname.setBounds(20, 70, 100, 20);
+        lblFirstname.setFont(new Font("Arial", Font.BOLD, 15));
 
         lblStudentnum = new JLabel("Student No.");
-        lblStudentnum.setBounds(20, 50, 100, 20);
+        lblStudentnum.setBounds(20, 100, 100, 20);
         lblStudentnum.setFont(new Font("Arial", Font.BOLD, 15));
 
         lblAge = new JLabel("Age");
-        lblAge.setBounds(20, 80, 100, 20);
+        lblAge.setBounds(20, 130, 100, 20);
         lblAge.setFont(new Font("Arial", Font.BOLD, 15));
 
         lblCourse = new JLabel("Course");
-        lblCourse.setBounds(20, 110, 100, 20);
+        lblCourse.setBounds(20, 160, 100, 20);
         lblCourse.setFont(new Font("Arial", Font.BOLD, 15));
 
-        txtfldName.setBounds(120, 20, 150, 20);
-        txtfldName.setFont(new Font("Arial", Font.PLAIN, 15));
+        txtfldSurname.setBounds(120, 40, 150, 20);
+        txtfldSurname.setFont(new Font("Arial", Font.PLAIN, 15));
 
-        txtfldStudentnum.setBounds(120, 50, 150, 20);
+        txtfldFirstname.setBounds(120, 70, 150, 20);
+        txtfldFirstname.setFont(new Font("Arial", Font.PLAIN, 15));
+
+        txtfldStudentnum.setBounds(120, 100, 150, 20);
         txtfldStudentnum.setFont(new Font("Arial", Font.PLAIN, 15));
 
-        txtfldAge.setBounds(120, 80, 50, 20);
+        txtfldAge.setBounds(120, 130, 50, 20);
         txtfldAge.setFont(new Font("Arial", Font.PLAIN, 15));
 
-        txtfldCourse.setBounds(120, 110, 150, 20);
+        txtfldCourse.setBounds(120, 160, 150, 20);
         txtfldCourse.setFont(new Font("Arial", Font.PLAIN, 15));
 
         btnSubmit = new JButton("Submit");
-        btnSubmit.setBounds(100, 150, 200, 30);
+        btnSubmit.setBounds(100, 200, 200, 30);
         btnSubmit.setFont(new Font("Arial", Font.BOLD, 15));
 
-        add(lblName);
+        add(lblTitle);
+        add(lblSurname);
+        add(lblFirstname);
         add(lblStudentnum);
         add(lblAge);
         add(lblCourse);
-        add(txtfldName);
+        add(txtfldSurname);
+        add(txtfldFirstname);
         add(txtfldStudentnum);
         add(txtfldAge);
         add(txtfldCourse);
@@ -68,18 +84,18 @@ public class Student extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String studentnum = txtfldStudentnum.getText();
-                String name = txtfldName.getText();
+                String surname = txtfldSurname.getText();
+                String firstname = txtfldFirstname.getText();
                 byte age = Byte.parseByte(txtfldAge.getText());
                 String course = txtfldCourse.getText();
 
                 System.out.println("Submitted student information: ");
                 System.out.println("Student Number: " + studentnum);
-                System.out.println("Name: " + name);
+                System.out.println("Surname: " + surname);
+                System.out.println("First Name: " + firstname);
                 System.out.println("Age: " + age);
                 System.out.println("Course: " + course);
             }
         });
     }
-
-   
 }
