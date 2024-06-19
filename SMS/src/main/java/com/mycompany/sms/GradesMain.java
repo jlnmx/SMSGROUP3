b63bf1b973ae125c347019d18186ef285d3e6ee6
jerickbryan <166296301+jerickbryan@users.noreply.github.com>
2024-06-19 16:36:17 +0800

    GradesMain.java

--- SMS/src/main/java/com/mycompany/sms/GradesMain.java
+++ SMS/src/main/java/com/mycompany/sms/GradesMain.java
@@ -1,10 +1,22 @@
 
 package com.mycompany.sms;
 
+import java.awt.EventQueue;
+
 public class GradesMain {
 
     public static void main(String[] args) {
-        new Grades();
+        EventQueue.invokeLater(new Runnable(){
+            @Override
+            public void run() {
+              Grades gr = new Grades ();
+              gr.setVisible(true);
+                
+            }
+            
+        });
+        
+      
     }
     
 }
